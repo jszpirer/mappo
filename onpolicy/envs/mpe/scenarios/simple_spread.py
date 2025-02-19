927a5395480aa9e81564a29e6314fd8dc444398a
jszpirer <jeanne.szpirer@ulb.be>
2025-02-19 16:25:21 +0100

    Before GPU

--- onpolicy/envs/mpe/scenarios/simple_spread.py
+++ onpolicy/envs/mpe/scenarios/simple_spread.py
@@ -40,11 +40,11 @@ class Scenario(BaseScenario):
 
         # set random initial states
         for agent in world.agents:
-            agent.state.p_pos = np.random.uniform(-1, +1, world.dim_p)
+            agent.state.p_pos = np.random.uniform(-3.85, +3.85, world.dim_p)
             agent.state.p_vel = np.zeros(world.dim_p)
             agent.state.c = np.zeros(world.dim_c)
         for i, landmark in enumerate(world.landmarks):
-            landmark.state.p_pos = 0.8 * np.random.uniform(-1, +1, world.dim_p)
+            landmark.state.p_pos = 0.8 * np.random.uniform(-3.85, +3.85, world.dim_p)
             landmark.state.p_vel = np.zeros(world.dim_p)
 
     def benchmark_data(self, agent, world):
@@ -103,5 +103,5 @@ class Scenario(BaseScenario):
                 continue
             comm.append(other.state.c)
             other_pos.append(other.state.p_pos - agent.state.p_pos)
-        #return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos)
-        return np.concatenate([agent.state.p_vel] + entity_pos + other_pos)
+        return np.concatenate([agent.state.p_vel] + [agent.state.p_pos] + entity_pos + other_pos)
+        #return np.concatenate([agent.state.p_vel] + entity_pos + other_pos)
