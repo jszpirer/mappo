31194a65c8005501834a03abad7f34a00787914c
jszpirer <jeanne.szpirer@ulb.be>
2026-03-19 00:26:54 +0100

    Updates on Thales

--- onpolicy/scripts/train_mpe_scripts/train_mpe_aggregation_attention.sh
+++ onpolicy/scripts/train_mpe_scripts/train_mpe_aggregation_attention.sh
@@ -6,7 +6,7 @@ num_agents=10
 nb_additional_data=2
 noise=0
 algo="rmappo" #"mappo" "ippo"
-exp="local_coverage_aggregation_attention_bigrange_nodirection_noffn_critic"
+exp="local_coverage_aggregation_attention_rangedirection_attentionactor"
 seed_max=5
 project="epuck_aggregation_10agents"
 
@@ -19,6 +19,6 @@ do
     --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} \
     --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 1 --episode_length 125 --num_env_steps 50000000 \
     --ppo_epoch 10 --use_ReLU --gain 0.01 --lr 7e-5 --critic_lr 7e-4 --user_name "jeanne-szpirer-universit-libre-de-bruxelles" --project_name ${project} \
-    --nb_additional_data ${nb_additional_data} --wheel_noise ${noise} --attention_actor --attention_critic \
-    --save_interval 1000000 --omniscient_critic
+    --nb_additional_data ${nb_additional_data} --wheel_noise ${noise} --attention_actor \
+    --save_interval 1000000 
 done
